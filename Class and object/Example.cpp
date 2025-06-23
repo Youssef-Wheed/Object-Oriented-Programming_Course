bbf3ea89044d934abb63c80dd58c6410a278d7a4
Youssef-Wheed <wayoussef138@gmail.com>
2025-06-23 17:15:23 +0300

    modeiy Example file

---
+++ Class and object/Example.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+// Define a class named Book
+class Book
+{
+public:
+    int id;
+    string title;
+    string author;
+    int pages;
+};
+
+int main()
+{
+    // Create an object of the Book class
+    Book book1;
+
+    // Assign values to the object's attributes
+    book1.id = 1;
+    book1.title = "Algorithms";
+    book1.author = "George Orwell";
+    book1.pages = 328;
+
+    // Display the object's attributes
+    cout << "Book ID: " << book1.id << endl;
+    cout << "Title: " << book1.title << endl;
+    cout << "Author: " << book1.author << endl;
+    cout << "Pages: " << book1.pages << endl;
+
+    return 0;
+}   
