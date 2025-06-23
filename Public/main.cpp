3094000ebccb0978069ff82ee9b5e9659f30350e
Youssef-Wheed <wayoussef138@gmail.com>
2025-06-23 18:17:53 +0300

    Refactor code structure for improved readability and maintainability

--- Public/main.cpp
+++ Public/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 using namespace std;
 
-class A
+// Defining a class named Book
+class Book
 {
-    
+    // Access specifier for the class attributes
+public:
+    // Attributes of the Book class
+    int price;
+    int pages;
+    string name;
 };
 
 int main()
 {
+    // Creating an instance of Book and initializing its attributes
+    // Using the public access specifier to access attributes directly  
+    Book book1;
+    book1.price = 500;
+    book1.pages = 300;
+    book1.name = "C++ Programming";
+
+    // Displaying book details
+    cout << "Book Name: " << book1.name << endl;
+    cout << "Book Price: " << book1.price << endl;
+    cout << "Book Pages: " << book1.pages << endl;
+    return 0;
 }
